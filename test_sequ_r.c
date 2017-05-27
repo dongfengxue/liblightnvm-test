b11724a1e1fafcc7e223be5fc5b89f1e4d4b0538
dongfengxue <1738761054@qq.com>
2017-05-27 11:55:52 +0800

    Update test_sequ_r.c

--- test_sequ_r.c
+++ test_sequ_r.c
@@ -53,9 +53,17 @@ void nvm_buf_fills1(char *buf,  int size)
     for (int  i = 0; i < size; ++i)
         buf[i] = '1';
 }
+void nvm_buf_fills01(char *buf,  int size)
+{
+    for (int  i = 0; i < size/2; ++i)
+        buf[i] = '1';
+    for (int  i = size/2; i < size; ++i)
+        buf[i] = '0';
+}
 int test_sequ_r(){
     char *buf_r=NULL,*buf_w=NULL;
-    struct nvm_addr addrs[2];
+    const int naddrs = geo->nplanes * geo->nsectors;
+    struct nvm_addr addrs[naddrs];
     struct nvm_ret ret;               //返回状态
     ssize_t res;
     size_t  buf_r_nbytes,buf_w_nbytes;
@@ -78,84 +86,57 @@ int test_sequ_r(){
         printf("erase error");
     }
     //write
-    buf_w_nbytes = geo->page_nbytes;        //开辟一个页大小的缓存
+    buf_w_nbytes = 2*geo->page_nbytes;        //开辟一个页大小的缓存
     buf_w= nvm_buf_alloc(geo, buf_w_nbytes);	// Setup buffers
     if (!buf_w) {
         printf("error!cant alloc buf_w!\n");
         free(buf_w);
     }
-    nvm_buf_fills0(buf_w, buf_w_nbytes);
+    nvm_buf_fills01(buf_w, buf_w_nbytes);
     //第一个页
-     for(int i=0;i<2;i++)   //两个plane的第一个page
-     {
-         addrs[i].ppa = blk_addr.ppa;
-         addrs[i].g.pg = 1;
-         addrs[i].g.sec = i % geo->nsectors;
-         addrs[i].g.pl = (i / geo->nsectors) % geo->nplanes;
+   for(int pg=0;pg<2;pg++) {
+       for (int i = 0; i < naddrs; i++)   //两个plane的第一个page
+       {
+           addrs[i].ppa = blk_addr.ppa;
+           addrs[i].g.pg = pg;
+           addrs[i].g.sec = i % geo->nsectors;
+           addrs[i].g.pl = (i / geo->nsectors) % geo->nplanes;
 
-     }
-    res = nvm_addr_write(dev, addrs, 1, buf_w, NULL, pmode, &ret);
+       }
+       res = nvm_addr_write(dev, addrs, naddrs, buf_w, NULL, pmode, &ret);
+   }
 
-//第二个页
-    for(int i=0;i<2;i++)   //两个plane的第一个page
-    {
-        addrs[i].ppa = blk_addr.ppa;
-        addrs[i].g.pg = 2;
-        addrs[i].g.sec = i % geo->nsectors;
-        addrs[i].g.pl = (i / geo->nsectors) % geo->nplanes;
-
-    }
-    char *buf_w1=NULL;
-    buf_w1= nvm_buf_alloc(geo, buf_w_nbytes);
-    if (!buf_w1) {
-        printf("error!cant alloc buf_w1!\n");
-        free(buf_w1);
-    }
-    nvm_buf_fills1(buf_w1, buf_w_nbytes);
-    res = nvm_addr_write(dev, addrs, 1, buf_w1, NULL, pmode, &ret);
 
     //read
-    buf_r_nbytes = geo->page_nbytes;
+    buf_r_nbytes = 2*geo->page_nbytes;
     buf_r = nvm_buf_alloc(geo, buf_r_nbytes);// Setup buffers
     if (!buf_r) {
         printf("buf_r alloc error!\n");
         return 1;
     }
-    //memset(buf_r, 0, buf_r_nbytes);
+  
     //读第一个页
-    for (int i = 0; i <2; ++i) {
+    for(int pg=0;pg<2;pg++) {
+        for (int i = 0; i < naddrs; ++i) {
             addrs[i].ppa = blk_addr.ppa;
 
             addrs[i].g.pg = 1;
             addrs[i].g.pl = (i / geo->nsectors) % geo->nplanes;
             addrs[i].g.sec = i % geo->nsectors;
         }
-        res = nvm_addr_read(dev, addrs, 1, buf_r, NULL, pmode, &ret);
+        memset(buf_r, '0', buf_r_nbytes);
+        res = nvm_addr_read(dev, addrs, naddrs, buf_r, NULL, pmode, &ret);
         if (res < 0) {
             printf("read error!\n");
             return 1;
         }
-    //读第二个页
-    for (int i = 0; i <2; ++i) {
-        addrs[i].ppa = blk_addr.ppa;
+    }
 
-        addrs[i].g.pg = 2;
-        addrs[i].g.pl = (i / geo->nsectors) % geo->nplanes;
-        addrs[i].g.sec = i % geo->nsectors;
-    }
-    char *buf_r1;
-    buf_r1 = nvm_buf_alloc(geo, buf_r_nbytes);// Setup buffers
-    res = nvm_addr_read(dev, addrs, 1, buf_r1, NULL, pmode, &ret);
-   // nvm_ret_pr(&ret);
-    if (res < 0) {
-        printf("read error!\n");
-        return 1;
-    }
-  //if(strcmp(buf_w,buf_r)==0)
+
     printf("buf_w:%s\n",buf_w);
-    printf("buf_w1:%s\n",buf_w1);
+    
     printf("buf_r:%s\n",buf_r);
-    printf("buf_r1:%s\n",buf_r1);
+   
     return 0;
 }
 int main(){
